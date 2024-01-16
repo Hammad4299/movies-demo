04a22a993c3df28646a5552651289a324023f16a
hammad4299 <hammad4299@gmail.com>
2024-01-16 15:08:13 +0500

    init: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import type { PluginUtils } from "tailwindcss/types/config";
 
 const config: Config = {
   content: [
@@ -13,8 +14,46 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: ({ theme }: PluginUtils) => ({
+        appColor: {
+          100: "#ebcfcf",
+          200: "#d79f9f",
+          300: "#c46e6e",
+          400: "#b03e3e",
+          500: "#9c0e0e",
+          600: "#7d0b0b",
+          700: "#5e0808",
+          800: "#3e0606",
+          900: "#1f0303",
+        },
+        primary: "#9c0e0e",
+        primaryDark: "#7d0b0b",
+        secondary: "#c27803",
+        secondaryDark: "#9f580a",
+        textPrimary: "#030712",
+        success: "#22c55e",
+        successDark: "#228B22",
+        error: "#e02424",
+        appBg: "#f9fafc",
+        appBgDark: "#f9faf9",
+      }),
+      backgroundColor: ({ theme }: PluginUtils) => ({
+        primary: theme("colors.primary"),
+        secondary: theme("colors.secondary"),
+        success: theme("colors.success"),
+        error: theme("colors.error"),
+      }),
+      borderColor: ({ theme }: PluginUtils) => ({
+        primary: theme("colors.primary"),
+        secondary: theme("colors.secondary"),
+        success: theme("colors.success"),
+        error: theme("colors.error"),
+      }),
+      fontFamily: ({ theme }: PluginUtils) => ({
+        montserrat: ["Montserrat", "sans-serif"],
+      }),
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 }
 export default config
