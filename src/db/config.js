64e4ea8a0bf7adcc4c6252332489d5806b86f7dc
hammad4299 <hammad4299@gmail.com>
2024-01-16 22:58:10 +0500

    fix: config port

--- src/db/config.js
+++ src/db/config.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 module.exports = {
     development: {
-        url: process.env.DATABASE_URL,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
         database: process.env.DB_NAME,
-        port: process.env.DB_PORT,
+        port: +process.env.DB_PORT || 5432,
         dialect: "postgres",
     },
 };
