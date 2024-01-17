edcff3a7c50de929f61f8c7f98030be46fa40e80
hammad4299 <hammad4299@gmail.com>
2024-01-17 16:25:59 +0500

    fix: password field dot size

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -12,7 +12,9 @@ export const InputComponent: React.FC<Props> = ({
             <input
                 className={`mb-1 text-white rounded-[10px] px-3 py-2 bg-inputColor text-bodySmall font-normal w-full border-inputColor placeholder-[rgba(255,255,255,0.8)] focus:bg-white focus:text-inputColor focus:placeholder-inputColor focus:border-inputColor border-solid border-[2px] duration-300 ${
                     error && `!border-error !text-error !placeholder-error`
-                } ${className}`}
+                } ${className} ${
+                    props.type == "password" && "font-['Verdana'] tracking-wide"
+                }`}
                 {...props}
             />
             <div className={`px-2 h-4 w-full text-[12px] text-error`}>
